ced780126fbe65b04633e2c7a89a2b91412b9cf8
felipevalbuena13 <juanfelipevalbuenavalderrama@gmail.com>
2024-04-18 21:39:04 -0500

    a;adir publicidad en home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import Script from "next/script";
+import Head from "next/head";
 export default function Home() {
   return (
     <main className="w-full px-24">
+      <Head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6712931388415461"
+          crossOrigin="anonymous"
+        ></Script>
+      </Head>
       <h1 className="text-blue-500 dark:text-orange-400">felipene</h1>
     </main>
   );
